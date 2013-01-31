2c7850f6a2723c5300f9403caf621b48a86253d0
JP Toto <james.p.toto@gmail.com>
2013-01-31 11:03:43 -0500

    Smarter nuget packing batch file

--- pack-nuget.bat
+++ pack-nuget.bat
@@ -1,3 +1,4 @@
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe src\log4net.ElasticSearch.sln /t:Clean,Rebuild /p:Configuration=Release /fileLogger
 copy LICENSE bin
 copy readme.txt bin
 NuGet.exe pack log4net.ElasticSearch.nuspec -Basepath bin
