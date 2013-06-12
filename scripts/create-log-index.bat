249c9361523c21fcbb901a9e2f1e963ee3441892
Nicholas Canzoneri <canzoneri.nicholas@gmail.com>
2013-06-11 20:28:56 -0400

    Adding explanatory messages to output of create-log-index.bat

--- scripts/create-log-index.bat
+++ scripts/create-log-index.bat
@@ -1,3 +1,6 @@
+echo "Attempting to delete log index (OK if not found)"
 curl -X DELETE http://localhost:9200/log
-
+echo ""
+echo "Attempting to create log index"
 curl -X POST http://localhost:9200/log -d @log-index-spec.json
+echo ""
