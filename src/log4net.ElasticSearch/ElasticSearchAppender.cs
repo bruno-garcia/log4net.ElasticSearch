f59266c791d816d8bc0b90c9a6dbee95e805b938
Uriel Hai <urielha@users.noreply.github.com>
2014-07-20 00:46:01 +0300

    move to another file

--- src/log4net.ElasticSearch/ElasticSearchAppender.cs
+++ src/log4net.ElasticSearch/ElasticSearchAppender.cs
@@ -7,42 +7,12 @@ using System.Linq;
 using System.Linq.Expressions;
 using log4net.ElasticSearch.Models;
 using log4net.Layout;
-using log4net.Util;
 using Nest;
 using log4net.Appender;
 using log4net.Core;
 
 namespace log4net.ElasticSearch
 {
-    public class TemplateInfo : IOptionHandler
-    {
-        private readonly IErrorHandler _errorHandler;
-
-        public string Name { get; set; }
-        public string FileName { get; set; }
-        public bool IsValid { get; private set; }
-
-        public TemplateInfo()
-        {
-            IsValid = false;
-            _errorHandler = new OnlyOnceErrorHandler(this.GetType().Name);
-        }
-
-        public void ActivateOptions()
-        {
-            if (string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(FileName))
-            {
-                _errorHandler.Error("Template name or fileName is empty!");
-            }
-
-            if (!File.Exists(FileName))
-            {
-                _errorHandler.Error(string.Format("Could not find template file: {0}", FileName));
-            }
-
-            IsValid = true;
-        }
-    }
     public class ElasticSearchAppender : AppenderSkeleton
     {
         private ElasticClient _client;
--- src/log4net.ElasticSearch/IElasticOption.cs
+++ src/log4net.ElasticSearch/IElasticOption.cs
@@ -1,5 +1,3 @@
-using System.Security.Cryptography.X509Certificates;
-
 namespace log4net.ElasticSearch
 {
     public interface IElasticOption
