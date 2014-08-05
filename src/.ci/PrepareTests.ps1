f02d9cb33b7cd846c64a702044148dfabe3c520d
Uriel Hai <urielha@users.noreply.github.com>
2014-08-05 18:58:22 +0300

    update .ci

--- src/.ci/PrepareTests.ps1
+++ src/.ci/PrepareTests.ps1
@@ -1,13 +1,15 @@
 # download elasticsearch:
 $clnt = new-object System.Net.WebClient
 $url = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.1.zip"
-$file = ".\elasticsearch-1.3.1.zip"
+$file = (Get-Location).Path + "\elasticsearch-1.3.1.zip"
 $clnt.DownloadFile($url,$file)
 
 # Unzip the file to specified location
 $shell_app=new-object -com shell.application 
 $zip_file = $shell_app.namespace($file) 
-$destination = $shell_app.namespace("ElasticSearchDirectory") 
+
+rmdir elasticsearch-1.3.1
+$destination = $shell_app.namespace((Get-Location).Path) 
 $destination.Copyhere($zip_file.items())
 
-Start-Job -ScriptBlock {ElasticSearchDirectory\bin\elasticsearch.bat}
+Start-Job -ScriptBlock {elasticsearch-1.3.1\bin\elasticsearch.bat}
--- src/.ci/PrepareTests.sh
+++ src/.ci/PrepareTests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -x
+#!/bin/bash
 
 # download elasticsearch and unzip:
 wget "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.1.zip"
