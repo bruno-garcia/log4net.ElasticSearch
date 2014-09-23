d8375b5fee55a40b16c7d42c20636a78f8a7b361
Joe Campbell <joecampbell1974@gmail.com>
2014-09-23 07:19:00 +0100

    Build resolves solution path

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,16 +1,19 @@
-task default -depends Clean, Compile, Test
+properties {
+    $base_dir   = (Get-Item (Resolve-Path .)).Parent.FullName
+    $sln_path   = "$base_dir\src\log4net.ElasticSearch.sln"
+}
 
-$solution = "C:\GitHub\log4net.ElasticSearch\src\log4net.ElasticSearch.sln"
+task default -depends Clean, Compile, Test
 
 task Clean {
     exec {
-        msbuild $solution /t:Clean /v:Quiet /nologo
+        msbuild $sln_path /t:Clean /v:Quiet /nologo
     }
 }
 
 task Compile {
     exec {
-        msbuild $solution /t:Build /v:Quiet /nologo
+        msbuild $sln_path /t:Build /v:Quiet /nologo
     }
 }
 
