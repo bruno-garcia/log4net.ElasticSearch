bbe166478f670e3f67b0f1bedb5ef078359672d7
Joe Campbell <joecampbell1974@gmail.com>
2014-09-23 08:04:31 +0100

    Build recreates bin directory prior to Rebuild

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,21 +1,33 @@
 properties {
     $base_dir   = (Get-Item (Resolve-Path .)).Parent.FullName
+    $bin_dir    = "$base_dir\bin"
     $sln_path   = "$base_dir\src\log4net.ElasticSearch.sln"
     $tests_path = "$base_dir\src\log4net.ElasticSearch.Tests\bin\Debug\log4net.ElasticSearch.Tests.dll"
     $xunit_path = "$base_dir\src\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
+    $dirs       = @($bin_dir)
 }
 
-task default -depends Clean, Compile, Test
+task default        -depends Clean, Compile, Test
 
 task Clean {
-    exec {
-        msbuild $sln_path /t:Clean /v:Quiet /nologo
+    $dirs | % {
+        $directory = $_
+
+        if (Test-Path $directory) {
+            Write-Host -NoNewline  "`tDeleting $directory"
+            Remove-Item $directory -Recurse -Force | out-null
+            Write-Host "...Done"
+        }
+
+        Write-Host -NoNewline  "`tCreating $directory"
+        New-Item $directory -Type Directory | out-null
+        Write-Host "...Done"
     }
 }
 
 task Compile {
     exec {
-        msbuild $sln_path /t:Build /v:Quiet /nologo
+        msbuild $sln_path /t:Rebuild /v:Quiet /nologo
     }
 }
 
