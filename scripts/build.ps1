1a52575c7775e6acc6a2e18acf212413f106f055
Joe Campbell <joecampbell1974@gmail.com>
2014-09-23 22:40:46 +0100

    Added Package task

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -2,13 +2,16 @@ properties {
     $base_dir   = (Get-Item (Resolve-Path .)).Parent.FullName
     $bin_dir    = "$base_dir\bin"
     $sln_path   = "$base_dir\src\log4net.ElasticSearch.sln"
-    $config     = "Debug"
+    $config     = "Release"
     $tests_path = "$base_dir\src\log4net.ElasticSearch.Tests\bin\$config\log4net.ElasticSearch.Tests.dll"
     $xunit_path = "$base_dir\src\packages\xunit.runners.1.9.2\tools\xunit.console.clr4.exe"
     $dirs       = @($bin_dir)
+    $artefacts  = @("$base_dir\LICENSE", "$base_dir\readme.txt")
+    $nuget_path = "NuGet.exe"
 }
 
 task default        -depends Clean, Compile, Test
+task Package        -depends default, CopyArtefactsToBinDirectory, CreateNugetPackage
 
 task Clean {
     $dirs | % { Recreate-Directory $_ }
@@ -26,6 +29,16 @@ task Test {
     }
 }
 
+task CopyArtefactsToBinDirectory {
+    $artefacts | % { CopyTo-BinDirectory $_ }
+}
+
+task CreateNugetPackage {
+    exec {
+        & NuGet.exe pack log4net.ElasticSearch.nuspec -Basepath bin
+    }
+}
+
 task ? -Description "Helper to display task info" {
     Write-Documentation
 }
@@ -40,5 +53,11 @@ function Recreate-Directory($directory) {
 
     Write-Host -NoNewline  "`tCreating $directory"
     New-Item $directory -Type Directory | out-null
+    Write-Host "...Done"
+}
+
+function CopyTo-BinDirectory($artefact) {
+    Write-Host -NoNewline  "`tCopying $artefact to $bin_dir"
+    Copy-Item $artefact $bin_dir
     Write-Host "...Done"
 }
