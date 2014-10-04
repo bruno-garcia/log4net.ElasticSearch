cbfab7f6701550985f69ea233f885c9cbc612575
Joe Campbell <joecampbell1974@gmail.com>
2014-10-04 14:59:51 +0100

    Test to assert properties now running; local thread asserts commented out

--- src/log4net.ElasticSearch.Tests/ElasticSearchAppenderTests.cs
+++ src/log4net.ElasticSearch.Tests/ElasticSearchAppenderTests.cs
@@ -10,20 +10,14 @@ namespace log4net.ElasticSearch.Tests
         private static readonly ILog _log = LogManager.GetLogger(typeof(ElasticSearchAppenderTests));
 
         [Fact]
-        public void Can_read_properties()
+        public void Global_context_properties_are_logged()
         {
             const string globalPropertyName = "globalProperty";
-            const string threadPropertyName = "threadProperty";
-            const string localThreadPropertyName = "logicalThreadProperty";
 
             var globalProperty = Faker.Lorem.Sentence(2);
-            var threadProperty = Faker.Lorem.Sentence(2);
-            var localTreadProperty = Faker.Lorem.Sentence(2);
             var message = Faker.Lorem.Words(1).First();
 
             GlobalContext.Properties[globalPropertyName] = globalProperty;
-            ThreadContext.Properties[threadPropertyName] = threadProperty;
-//            LogicalThreadContext.Properties[localThreadPropertyName] = localTreadProperty;
 
             _log.Info(message);
 
@@ -36,9 +30,43 @@ namespace log4net.ElasticSearch.Tests
                     var firstEntry = searchResults.Documents.First();
 
                     firstEntry.Properties[globalPropertyName].Should().Be(globalProperty);
-                    firstEntry.Properties[threadPropertyName].Should().Be(threadProperty);
-//                    firstEntry.Properties["logicalThreadDynamicProperty"].Should().Be(localTreadProperty);
                 });
+        }
+
+        [Fact]
+        public void Thread_context_properties_are_logged()
+        {
+            const string threadPropertyName = "threadProperty";
+
+            var threadProperty = Faker.Lorem.Sentence(2);
+            var message = Faker.Lorem.Words(1).First();
+
+            ThreadContext.Properties[threadPropertyName] = threadProperty;
+
+            _log.Info(message);
+
+            Retry.Ignoring<AssertException>(() =>
+                {
+                    var searchResults = client.Search<LogEvent>(s => s.Query(q => q.Term(@event => @event.Message, message)));
+
+                    searchResults.Total.Should().Be(1);
+
+                    var firstEntry = searchResults.Documents.First();
+
+                    firstEntry.Properties[threadPropertyName].Should().Be(threadProperty);
+                });
+        }
+
+        [Fact(Skip = "LogicalThreadContext properties cause SerializationException")]
+        public void Local_thread_context_properties_cause_error()
+        {
+            const string localThreadPropertyName = "logicalThreadProperty";
+            var localTreadProperty = Faker.Lorem.Sentence(2);
+            var message = Faker.Lorem.Words(1).First();
+
+            LogicalThreadContext.Properties[localThreadPropertyName] = localTreadProperty;
+
+            _log.Info(message);
         }
 
         [Fact]
