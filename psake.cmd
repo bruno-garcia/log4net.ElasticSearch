542b91ccdb38e1d57974827d8458b3052e7ceee3
JP Toto <james.p.toto@gmail.com>
2014-11-07 12:04:18 -0500

    Add packages folder to .gitignore, add package restore command to cmd build file

--- psake.cmd
+++ psake.cmd
@@ -4,6 +4,8 @@ if '%1'=='/?' goto help
 if '%1'=='-help' goto help
 if '%1'=='-h' goto help
 
+tools\nuget\nuget.exe restore src\log4net.ElasticSearch.sln
+
 powershell -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0\src\packages\psake.4.3.2\tools\psake.ps1' '%~dp0\scripts\build.ps1' %*; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }"
 exit /B %errorlevel%
 
