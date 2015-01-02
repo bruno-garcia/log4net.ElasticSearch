1ba33674f4e0034eaad524476a0c9c2eb6b077df
mick delaney <mickdelaney@gmail.com>
2015-01-02 15:32:32 +0000

    No Scheme Defaults To Http

--- src/log4net.ElasticSearch.Tests/UnitTests/SslTests.cs
+++ src/log4net.ElasticSearch.Tests/UnitTests/SslTests.cs
@@ -9,19 +9,27 @@ namespace log4net.ElasticSearch.Tests.UnitTests
 {
     public class SslTests : IDisposable
     {
-        const string ConnectionString = "Scheme=https;Server=localhost;Index=log_test;Port=9200;rolling=true";
-
         public void Dispose()
         {
             
         }
 
         [Fact]
-        public void When_number_of_LogEvents_is_less_than_Buffer_nothing_is_sent_to_ElasticSearch()
+        public void When_a_https_scheme_is_provided_it_should_create_a_URI_with_https()
         {
+            string ConnectionString = "Scheme=https;Server=localhost;Index=log_test;Port=9200;rolling=true";
             var uriBuilder = log4net.ElasticSearch.Models.Uri.Create(ConnectionString);
             Uri uri = uriBuilder;
             uri.Scheme.Should().Be("https");
+        }
+
+        [Fact]
+        public void When_no_scheme_is_provided_it_should_create_a_URI_with_http()
+        {
+            string ConnectionString = "Server=localhost;Index=log_test;Port=9200;rolling=true";
+            var uriBuilder = log4net.ElasticSearch.Models.Uri.Create(ConnectionString);
+            Uri uri = uriBuilder;
+            uri.Scheme.Should().Be("http");
         }
     }
 }
