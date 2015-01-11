2e4589251f8f3a003af16e17beeabbcf4e2b5758
JP Toto <james.p.toto@gmail.com>
2015-01-11 07:45:42 -0500

    Correct version info

--- src/log4net.ElasticSearch/AssemblyVersionInfo.cs
+++ src/log4net.ElasticSearch/AssemblyVersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.1.0-beta")]
-[assembly: AssemblyFileVersion("2.1.0-beta")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
