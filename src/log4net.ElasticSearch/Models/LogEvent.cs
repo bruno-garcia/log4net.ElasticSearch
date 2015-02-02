3108169bc435f56c9eaa630caef7095572c3ddbc
ttingen <ttingen@gmail.com>
2015-02-02 10:22:54 -0500

    Updated handling of the MessageObject if it's an Exception type.

--- src/log4net.ElasticSearch/Models/LogEvent.cs
+++ src/log4net.ElasticSearch/Models/LogEvent.cs
@@ -63,7 +63,6 @@ namespace log4net.ElasticSearch.Models
                 identity = loggingEvent.Identity,
                 threadName = loggingEvent.ThreadName,
                 userName = loggingEvent.UserName,
-                messageObject = loggingEvent.MessageObject != null && loggingEvent.MessageObject.GetType() != typeof(string) ? loggingEvent.MessageObject : new object(),
                 timeStamp = loggingEvent.TimeStamp.ToUniversalTime().ToString("O"),
                 exception = loggingEvent.ExceptionObject == null ? new object() : JsonSerializableException.Create(loggingEvent.ExceptionObject),
                 message = loggingEvent.RenderedMessage,
@@ -71,6 +70,24 @@ namespace log4net.ElasticSearch.Models
                 hostName = Environment.MachineName,
                 level = loggingEvent.Level == null ? null : loggingEvent.Level.DisplayName
             };
+
+            // Added special handling of the MessageObject since it may be an exception. 
+            // Exception Types require specialized serialization to prevent serialization exceptions.
+            if (loggingEvent.MessageObject != null && loggingEvent.MessageObject.GetType() != typeof(string))
+            {
+                if (loggingEvent.MessageObject is Exception)
+                {
+                    logEvent.messageObject = JsonSerializableException.Create((Exception)loggingEvent.MessageObject);
+                }
+                else
+                {
+                    logEvent.messageObject = loggingEvent.MessageObject;
+                }
+            }
+            else
+            {
+                logEvent.messageObject = new object();
+            }
 
             if (loggingEvent.LocationInformation != null)
             {
