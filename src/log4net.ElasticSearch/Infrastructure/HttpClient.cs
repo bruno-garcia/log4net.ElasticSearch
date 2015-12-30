2fa2371f30fb09db19b8cff9855bcfcf27a266b3
JP Toto <JP Toto>
2015-12-29 21:52:40 -0500

    Add a PostBulk method to support using the ES bulk API

--- src/log4net.ElasticSearch/Infrastructure/HttpClient.cs
+++ src/log4net.ElasticSearch/Infrastructure/HttpClient.cs
@@ -8,6 +8,7 @@ namespace log4net.ElasticSearch.Infrastructure
     public interface IHttpClient
     {
         void Post<T>(Uri uri, T item);
+        void PostBulk<T>(Uri uri, T items);
     }
 
     public class HttpClient : IHttpClient
@@ -25,6 +26,34 @@ namespace log4net.ElasticSearch.Infrastructure
                 streamWriter.Flush();
 
                 var httpResponse = (HttpWebResponse) httpWebRequest.GetResponse();
+                httpResponse.Close();
+
+                if (httpResponse.StatusCode != HttpStatusCode.Created)
+                {
+                    throw new WebException(
+                        "Failed to post {0} to {1}.".With(item.GetType().Name, uri));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Post the events to the Elasticsearch _bulk API for faster inserts
+        /// </summary>
+        /// <typeparam name="T">Type/item being inserted. Should be a list of events</typeparam>
+        /// <param name="uri">Fully formed URI to the bulk API</param>
+        /// <param name="item"></param>
+        public void PostBulk<T>(Uri uri, T items)
+        {
+            var bulkUri = new Uri(uri, "_bulk");
+
+            var httpWebRequest = RequestFor(bulkUri);
+
+            using (var streamWriter = GetRequestStream(httpWebRequest))
+            {
+                streamWriter.Write(item.ToJson());
+                streamWriter.Flush();
+
+                var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
                 httpResponse.Close();
 
                 if (httpResponse.StatusCode != HttpStatusCode.Created)
