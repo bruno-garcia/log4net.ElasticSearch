80dcba98a33121ba5926f2317f1ffed4b8161f8a
JP Toto <jtoto@emoneyadvisor.com>
2016-04-26 15:09:36 -0400

    Version updates

--- src/log4net.ElasticSearch/AssemblyVersionInfo.cs
+++ src/log4net.ElasticSearch/AssemblyVersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.3.2")]
-[assembly: AssemblyFileVersion("2.3.2")]
+[assembly: AssemblyVersion("2.3.3")]
+[assembly: AssemblyFileVersion("2.3.3")]
