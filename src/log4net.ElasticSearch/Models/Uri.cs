bbd86c6745e5c66c46b3af8b2ac3a69acc425ebc
Joao FERREIRA <jferreira@nomadvantage.com>
2018-06-06 20:25:08 +0200

    add routing parameter to connection string

--- src/log4net.ElasticSearch/Models/Uri.cs
+++ src/log4net.ElasticSearch/Models/Uri.cs
@@ -18,12 +18,12 @@ namespace log4net.ElasticSearch.Models
             if (!string.IsNullOrWhiteSpace(uri.User()) && !string.IsNullOrWhiteSpace(uri.Password()))
             {
                 return
-                    new System.Uri(string.Format("{0}://{1}:{2}@{3}:{4}/{5}/logEvent{6}", uri.Scheme(), uri.User(), uri.Password(),
-                                                 uri.Server(), uri.Port(), uri.Index(), uri.Bulk()));
+                    new System.Uri(string.Format("{0}://{1}:{2}@{3}:{4}/{5}/logEvent{6}{7}", uri.Scheme(), uri.User(), uri.Password(),
+                                                 uri.Server(), uri.Port(), uri.Index(), uri.Routing(), uri.Bulk()));
             }
             return string.IsNullOrEmpty(uri.Port())
-                ? new System.Uri(string.Format("{0}://{1}/{2}/logEvent{3}", uri.Scheme(), uri.Server(), uri.Index(), uri.Bulk()))
-                : new System.Uri(string.Format("{0}://{1}:{2}/{3}/logEvent{4}", uri.Scheme(), uri.Server(), uri.Port(), uri.Index(), uri.Bulk()));
+                ? new System.Uri(string.Format("{0}://{1}/{2}/logEvent{3}{4}", uri.Scheme(), uri.Server(), uri.Index(), uri.Routing(), uri.Bulk()))
+                : new System.Uri(string.Format("{0}://{1}:{2}/{3}/logEvent{4}{5}", uri.Scheme(), uri.Server(), uri.Port(), uri.Index(), uri.Routing(), uri.Bulk()));
         }
 
         public static Uri For(string connectionString)
@@ -56,14 +56,25 @@ namespace log4net.ElasticSearch.Models
             return connectionStringParts[Keys.Port];
         }
 
+        string Routing()
+        {
+            var routing = connectionStringParts[Keys.Routing];
+            if (!string.IsNullOrWhiteSpace(routing))
+            {
+                return $"?routing={routing}";
+            }
+
+            return string.Empty;
+        }
+
         string Bulk()
         {
             var bufferSize = connectionStringParts[Keys.BufferSize];
-            if (Convert.ToInt32(bufferSize)> 1)
+            if (Convert.ToInt32(bufferSize) > 1)
             {
                 return "/_bulk";
             }
-            
+
             return string.Empty;
         }
 
@@ -91,6 +102,7 @@ namespace log4net.ElasticSearch.Models
             public const string Index = "Index";
             public const string Rolling = "Rolling";
             public const string BufferSize = "BufferSize";
+            public const string Routing = "Routing";
         }
     }
 }
