64f575869deced4641572667387145d6eb44b51e
Bruno Garcia <bruno@brunogarcia.com>
2019-08-15 14:23:22 +0200

    let it generate asmInfo for tests

--- src/log4net.ElasticSearch.Tests/Properties/AssemblyInfo.cs
+++ src/log4net.ElasticSearch.Tests/Properties/AssemblyInfo.cs
@@ -1,17 +1,1 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using log4net.Config;
-
-[assembly: AssemblyTitle("log4net.ElasticSearch.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("log4net.ElasticSearch.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: Guid("5b0b6737-b33b-436e-aeab-33f32554855b")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: XmlConfigurator(ConfigFile = "logConfig.xml", Watch = true)]
+﻿[assembly: log4net.Config.XmlConfigurator(ConfigFile = "logConfig.xml", Watch = true)]
